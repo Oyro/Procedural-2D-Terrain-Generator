c9e68937c40a63114eb2b211f00110ac04dd2dd8
Oskar Lindkvist <Oskar.lindkvist@hotmail.com>
2019-09-16 23:01:47 +0200

    Add files via upload

---
+++ TerrainSegment.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class TerrainSegment : MonoBehaviour
+{
+    public MeshFilter meshFilter;
+    Mesh mesh;
+    //public TerrainGenerator terrainGenerator;
+    public float width;
+    public float endHeight;
+    public float segmentDeltaHeight;
+    public Vector2[] nodes;
+
+    private void Awake()
+    {
+        
+    }
+    
+    private void Start()
+    {
+        
+    }
+
+    public void InitMesh(Vector3[] verts, int[] tris)
+    {
+        mesh = meshFilter.mesh = new Mesh();
+        mesh.vertices = verts;
+        mesh.triangles = tris;
+    }
+}
